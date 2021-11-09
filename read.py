cdf9589c0feaa39301414bf00a68ab11a32c3d93
Yang <e102777tw@gmail.com>
2021-11-09 09:52:57 +0800

    Add files via upload

--- read.py
+++ read.py
@@ -59,11 +59,11 @@ def masking(img,mask):
 
 
 
-data_name = 'l11'
-data_path = r"D:\Solar panels\20211105\\" +data_name +"_RT"
+data_name = 'g_1'
+data_path = r"D:\Solar panels\20211026\\" +data_name +"_RT"
 img = envi.open(data_path + '.hdr',data_path + '.raw').asarray()
 w,h,b = img.shape
-# img_1d = img.reshape(-1,b)
+img_1d = img.reshape(-1,b)
 
 #%% train masking model
 
@@ -91,11 +91,11 @@ w,h,b = img.shape
 # img_masked = masking(img,mask)
 
 # roi_img=roi(img_masked,mask,400)
-# # plt.figure()
-# # plt.imshow(mask)
-# # plt.figure()
-# # plt.imshow(img_masked[:,:,101])
-# # plt.figure()
+# plt.figure()
+# plt.imshow(mask)
+# plt.figure()
+# plt.imshow(img_masked[:,:,101])
+# plt.figure()
 # for i in range(1,len(roi_img)+1):
 #     try:
 #         plt.subplot(10, 10, i)
@@ -134,7 +134,7 @@ gt = []
 for i,f in enumerate(files) :
     current_file = np.load(f)
     for cf in current_file:
-        average_hsi_sample.append(cf)
+        average_hsi_sample.append(cf[0:200])
         f = f.split('\\')[-1]
         if 'b' in  f:
             gt.append(0)
@@ -148,41 +148,47 @@ for i,f in enumerate(files) :
 #%%
 # 光譜前處理SNV    
 snv_ahs= average_hsi_sample
-# ahs = np.array(average_hsi_sample)
-# snv_ahs = snv(ahs)
+ahs = np.array(average_hsi_sample)
+snv_ahs = snv(ahs)
 
 # 平均光譜圖
 
 plt.figure()    
 for i in range(len(gt)):
     if  gt[i]==0:
-        plt.plot(range(b),snv_ahs[i],'b') 
+        cb,=plt.plot(range(200),snv_ahs[i],'b') 
     elif gt[i]==1:
-        plt.plot(range(b),snv_ahs[i],'r') 
+        cw,=plt.plot(range(200),snv_ahs[i],'r') 
     elif gt[i]==2:
-        plt.plot(range(b),snv_ahs[i],'g') 
+        l,=plt.plot(range(200),snv_ahs[i],'g') 
     elif gt[i]==3:
-        plt.plot(range(b),snv_ahs[i],'y') 
+        g,=plt.plot(range(200),snv_ahs[i],'y') 
+plt.legend([cb,cw,l,g],('chip_b','chip_w','line','glass'))
 # PCA 降維        
-pca = PCA(3)
+pca = PCA(5)
 pca_res = pca.fit_transform(snv_ahs)
 evr = sum(pca.explained_variance_ratio_)
 ev = pca.explained_variance_
 
+# 獲取eigen value
+from scipy import  linalg
+cov_mat = np.cov(snv_ahs.T)
+eigen_val = linalg.eigvals(cov_mat)
+eigen_val_sort = np.argsort(eigen_val)+0
 
-# SVM model 
+# # SVM model 
 
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+# from sklearn.svm import SVC
+# from sklearn.metrics import accuracy_score
+# from sklearn.model_selection import train_test_split
 
-train_smaple,test_smaple,train_gt,test_gt = train_test_split(pca_res,gt,test_size=0.5)
-svm = SVC()
-model = svm.fit(train_smaple,train_gt)
+# train_smaple,test_smaple,train_gt,test_gt = train_test_split(pca_res,gt,test_size=0.5,random_state=8787)
+# svm = SVC()
+# model = svm.fit(train_smaple,train_gt)
 
-# predict result
+# # predict result
 
-result = model.predict(test_smaple)
-acc = accuracy_score(test_gt,result)
-stack = np.stack((result,test_gt),axis=1)
+# result = model.predict(test_smaple)
+# acc = accuracy_score(test_gt,result)
+# stack = np.stack((result,test_gt),axis=1)
 
